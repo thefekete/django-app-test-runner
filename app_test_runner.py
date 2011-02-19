91e5fec1111d96914fb2bb02ea1991ac4ec42d6d
Kai <k@limist.com>
2011-02-19 17:15:37 -0500

    Minor PEP8 formatting changes.

--- app_test_runner.py
+++ app_test_runner.py
@@ -8,19 +8,20 @@ from optparse import OptionParser
 from django.conf import settings
 from django.core.management import call_command
 
+
 def main():
     """
     The entry point for the script. This script is fairly basic. Here is a
     quick example of how to use it::
-    
+
         app_test_runner.py [path-to-app]
-    
+
     You must have Django on the PYTHONPATH prior to running this script. This
     script basically will bootstrap a Django environment for you.
-    
+
     By default this script with use SQLite and an in-memory database. If you
     are using Python 2.5 it will just work out of the box for you.
-    
+
     TODO: show more options here.
     """
     parser = OptionParser()
@@ -29,9 +30,9 @@ def main():
     parser.add_option("--DATABASE_USER", dest="DATABASE_USER", default="")
     parser.add_option("--DATABASE_PASSWORD", dest="DATABASE_PASSWORD", default="")
     parser.add_option("--SITE_ID", dest="SITE_ID", type="int", default=1)
-    
+
     options, args = parser.parse_args()
-    
+
     # check for app in args
     try:
         app_path = args[0]
@@ -43,7 +44,7 @@ def main():
             app_path = app_path[:-1]
         parent_dir, app_name = os.path.split(app_path)
         sys.path.insert(0, parent_dir)
-    
+
     settings.configure(**{
         "DATABASE_ENGINE": options.DATABASE_ENGINE,
         "DATABASE_NAME": options.DATABASE_NAME,
@@ -54,10 +55,10 @@ def main():
         "TEMPLATE_LOADERS": (
             "django.template.loaders.filesystem.load_template_source",
             "django.template.loaders.app_directories.load_template_source",
-        ),
+            ),
         "TEMPLATE_DIRS": (
             os.path.join(os.path.dirname(__file__), "templates"),
-        ),
+            ),
         "INSTALLED_APPS": (
             # HACK: the admin app should *not* be required. Need to spend some
             # time looking into this. Django #8523 has a patch for this issue,
@@ -69,8 +70,8 @@ def main():
             "django.contrib.sessions",
             "django.contrib.sites",
             app_name,
-        ),
-    })
+            ),
+        })
     call_command("test")
 
 if __name__ == "__main__":
